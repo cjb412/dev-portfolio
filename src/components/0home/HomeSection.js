8db4bde7b3c0b682b26035a33b8a4bc3c5575546
cjb412 <cdfcblank@gmail.com>
2023-07-07 18:07:28 -0400

    Fixed home relative link, refactored intro to use css for sizing

--- src/components/0home/HomeSection.js
+++ src/components/0home/HomeSection.js
@@ -3,21 +3,8 @@ import IntroText from './HomeText'
 import { useState, useEffect } from 'react'
 
 const HomeSection = () => {
-    const [introHeight, setIntroHeight] = useState(window.innerHeight)
-
-    // DYNAMICALLY SIZE INTRO TO VIEWPORT HEIGHT
-    useEffect(() => {
-        const handleResize = () =>
-        {
-          console.log(window.innerHeight)
-          setIntroHeight(window.innerHeight)
-        }
-    
-        window.addEventListener('resize', handleResize)
-    })
-  
     return (
-    <div id={styles['home-section']} style={{height: introHeight}}>
+    <div id='home-section'>
         <IntroText />
     </div>
   )
